08812bd95437c6ff4ad7c89a2cf320ebcc2ae62f
Konstantin-Simakov <kvs.2000@mail.ru>
2020-08-27 12:49:30 +0300

    Add files via upload

---
+++ Chapter 2/ex4_v2.c
@@ -0,0 +1,30 @@
+/* ex4.c -- (Chapter 2) */
+#include <stdio.h>
+
+void jolly (void);
+void deny (void);
+
+int main (void)
+{
+	jolly();
+	jolly();
+	jolly();
+	
+	deny();
+
+	return 0;
+}
+
+void jolly (void)
+{
+	printf("He is a jolly fellow!\n");
+
+	return;
+}
+
+void deny (void)
+{
+	printf("Nobody can deny it!\n");
+
+	return;
+}
