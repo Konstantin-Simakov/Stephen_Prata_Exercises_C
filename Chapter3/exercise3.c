5c6dda6fe223c93ec5fd8dc656aa6ae07a62b336
Konstantin-Simakov <kvs.2000@mail.ru>
2020-09-17 17:31:17 +0300

    I add the missing chapters.

---
+++ Chapter3/exercise3.c
@@ -0,0 +1,11 @@
+/* exercise3.c -- Chapter 3 */
+#include <stdio.h>
+
+int main (void)
+{
+	printf("\a");
+	printf("Frightened by the sudden sound Vika screamed:\n");
+	printf("\"In the name of all stars, what was that!\"\n");
+
+	return 0;
+}
