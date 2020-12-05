98519059acf85eee5a823547ef0c2ecc8635e79c
Konstantin-Simakov <kvs.2000@mail.ru>
2020-12-05 12:03:22 +0300

    Add files via upload

---
+++ Chapter 6/exercise2.c
@@ -0,0 +1,18 @@
+/* exercise2.c -- Chapter 6 */
+#include <stdio.h>
+
+#define SIZE 5		// isosceles dollar-sign triangle -- output
+
+int main (void)
+{
+	int i, j;
+
+	for (i = 1; i <= SIZE; i++)
+	{
+		for (j = 1; j <= i; j++)
+			printf("$");
+		printf("\n");
+	}
+
+	return 0;
+}
