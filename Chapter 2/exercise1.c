e6fd2e3d2b66ad14d641cd7566c28ce356f8e5cd
Konstantin-Simakov <kvs.2000@mail.ru>
2021-10-10 16:03:14 +0300

    Update and rename ex1.c to exercise1.c

--- Chapter 2/exercise1.c
+++ Chapter 2/exercise1.c
@@ -1,9 +1,9 @@
-// ex1.c -- (Chapter 2);
+/* exercise1.c -- Chapter 2 */
 #include <stdio.h>
 
 int main(void)
 {
-	// Instead Ivan Ivanov may be written any other name.
+	/* instead Ivan Ivanov may be written any other name */
 	printf("Ivan Ivanov\n");
 	printf("Ivan\nIvanov\n");
 	printf("Ivan ");
