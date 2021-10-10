aee87f84f812093145e889fb3153f05366768616
Konstantin-Simakov <kvs.2000@mail.ru>
2021-10-10 16:30:26 +0300

    Update exercise6.c

--- Chapter 3/exercise6.c
+++ Chapter 3/exercise6.c
@@ -1,7 +1,7 @@
 /* exercise6.c -- Chapter 3 */
 #include <stdio.h>
 
-int main (void)
+int main(void)
 {
 	long double weight_m0 = 3.0e-23;	// gram
 	short int weight_water = 950;		// gram; short int type is allowed
