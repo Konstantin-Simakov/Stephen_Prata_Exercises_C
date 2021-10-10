aac5e253baa30a914bc64953982983e95b5e7729
Konstantin-Simakov <kvs.2000@mail.ru>
2021-10-10 16:16:46 +0300

    Update and rename ex7.c to exercise8.c

---
+++ Chapter 2/exercise8.c
@@ -0,0 +1,27 @@
+/* exercise8.c -- Chapter 2 */
+#include <stdio.h>
+
+void one_three(void);
+void two(void);
+
+int main(void)
+{
+	printf("Begin now:\n");
+	one_three();
+	printf("Excellent!\n");
+	
+	return 0;
+}
+
+/* here you can uncomment these comment below; nothing will change */
+void one_three(void)
+{
+	printf("One\n");
+	two();
+	printf("Three\n");
+}
+
+void two(void)
+{
+	printf("Two\n");
+}
