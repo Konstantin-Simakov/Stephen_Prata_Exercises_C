7b4cfbf768c4c0b93251c90b0cdd446e8d29bc34
Konstantin-Simakov <kvs.2000@mail.ru>
2021-10-10 16:08:44 +0300

    Update and rename ex3.c to exercise3.c

---
+++ Chapter 2/exercise3.c
@@ -0,0 +1,12 @@
+/* exercise3.c -- Chapter 2 */
+#include <stdio.h>
+
+int main(void)
+{
+	/* instead age = 23 may be written any other number */
+	int age = 23;
+
+	printf("Your age (in days) = %d", age * 365);
+	
+	return 0;
+}
