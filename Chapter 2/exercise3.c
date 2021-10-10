154519b6d6bd7895135a41ef52f3c0aca69d61f0
Konstantin-Simakov <kvs.2000@mail.ru>
2021-10-10 16:10:24 +0300

    Update exercise3.c

--- Chapter 2/exercise3.c
+++ Chapter 2/exercise3.c
@@ -4,9 +4,13 @@
 int main(void)
 {
 	/* instead age = 23 may be written any other number */
-	int age = 23;
+	int age;
+	int days;
+	
+	age = 23;
+	days = age * 365;
 
-	printf("Your age (in days) = %d", age * 365);
+	printf("Your age (in days) = %d", days);
 	
 	return 0;
 }
