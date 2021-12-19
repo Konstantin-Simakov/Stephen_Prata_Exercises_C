503920af07f525c1e586f4454fcabd21cd1b0aa2
Konstantin-Simakov <kvs.2000@mail.ru>
2021-12-19 16:03:07 +0300

    Update exercise9.c

--- Chapter 12/exercise9.c
+++ Chapter 12/exercise9.c
@@ -1,6 +1,7 @@
 /* exercise9.c -- Chapter 12 */
-#include <stdio.h>		/* for printf(), getchar(), puts(), scanf() */
-#include <stdlib.h>		/* for malloc(), free() */
+#include <stdio.h>		/* for printf(), getchar(), puts(), scanf()	 */
+#include <stdlib.h>		/* for malloc(), free() 			 */
+#include <string.h>		/* for strlen(), strcpy()			 */
 
 #define SIZE 40
 
@@ -8,26 +9,29 @@ int main(void)
 {
 	int i;
 	int num;
-	char * temp;
+	char temp[SIZE];
+	char * word;
 	char ** words;
 
 	printf("Enter words number: ");
 	scanf("%d", &num);
-	words = (char **) malloc(num * sizeof(char *));		// ??
-	
-	printf("Now enter %d words:\n", num);
+	words = (char **) malloc(num * sizeof(char *));		/* ??? */
+
+	printf("Now enter these %d words:\n", num);
 	for (i = 0; i < num; i++)
 	{
-		temp = (char *) malloc(SIZE * sizeof(char));		
 		scanf("%s", temp);
-		words[i] = temp;
+		word = (char *) malloc((strlen(temp) + 1) * sizeof(char));
+		strcpy(word, temp);		
+		words[i] = word;
 	}
 
 	printf("Here are all %d words you entered:\n", num);
 	for (i = 0; i < num; i++)
 		puts(words[i]);
 
-	free(words);	// <==> free(temp); for all earlier used temp variables
-	
+	free(words);	/* <==> free(word); for all previously created 	*/
+			/* 	dynamic char arrays named word		*/
+
 	return 0;
 }
