1d2f9cb57b468ff9fdb68716f252da3b1607785f
Konstantin_Simakov <kvs.2000@mail.ru>
2024-08-03 18:38:07 +0300

    Add files via upload

--- Chapter 5/exercise4.c
+++ Chapter 5/exercise4.c
@@ -1,8 +1,9 @@
 /* exercise4.c -- Chapter 5 */
 #include <stdio.h>
 
-const float FOOT_CM = 30.48f;       // cm number per one foot
-const int FOOT_INCH = 12;       // ineches number per one foot
+const float INCH_CM = 2.54f;                    // cm number per one inche
+const int FOOT_INCH = 12;                       // inches number per one foot
+const float FOOT_CM = INCH_CM * FOOT_INCH;      // cm number per one foot
 
 int main(void)
 {
@@ -12,20 +13,20 @@ int main(void)
     printf("This program converts cm number into feet and inches number.\n");
 
     printf("Please enter a height in cm "
-        "(or <= 0 for exit the program):\n");
+        "(or <= 0 to exit the program): ");
     scanf("%f", &cm);
     while (cm > 0)
     {
         foot = (int) (cm / FOOT_CM);
         inch_left = (cm / FOOT_CM - foot) * FOOT_INCH;          // == cm % FOOT_CM * FOOT_INCH; if cm has been int type
 
-        printf("%.2f cm = %d feet, %.1f inches.\n", cm, foot, inch_left);
+        printf("%.1f cm = %d feet, %.1f inches\n", cm, foot, inch_left);
 
         printf("Please enter a height in cm "
-            "(or <= 0 for exit the program)\n");
+            "(or <= 0 to exit the program): ");
         scanf("%f", &cm);
     }
-    printf("That's all. Good bye!\n");
+    printf("That\'s all. Good bye!\n");
 
     return 0;
 }
